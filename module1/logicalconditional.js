e352df88da36b8f729f6604e3541cef8b1d8e490
karthickroy <146054668+karthickroy@users.noreply.github.com>
2023-09-26 12:24:37 +0530

    Add files via upload

---
+++ module1/logicalconditional.js
@@ -0,0 +1,22 @@
+// var age = 24;
+
+// var fname = 'yes';
+
+// if(age >18 && fname ==="yes"){
+//     console.log("hello");
+// }else{
+//     console.log("bye");
+// }
+// if() {
+// }
+// else if() ()
+// else()
+var salary = 4000;
+var employee = "raj";
+
+if(employee === "raj" && salary > 5000){
+    console.log("increment");
+} else {
+    console.log("decrement");
+}
+
